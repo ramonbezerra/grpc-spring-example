5342276d370c0d0552b7be57bbe400644451d18f
ramonbezerra <ramon.bezerra@outlook.com>
2023-12-21 00:28:29 -0300

    configure proto file for second microservice

---
+++ grpc-customer-example/src/main/proto_gen/br/example/demo/model/CustomerServiceGrpc.java
@@ -0,0 +1,293 @@
+package br.example.demo.model;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.60.0)",
+    comments = "Source: customer.proto")
+@io.grpc.stub.annotations.GrpcGenerated
+public final class CustomerServiceGrpc {
+
+  private CustomerServiceGrpc() {}
+
+  public static final java.lang.String SERVICE_NAME = "model.CustomerService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<com.google.protobuf.Empty,
+      br.example.demo.model.CustomerProto.Customers> getFindAllMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "FindAll",
+      requestType = com.google.protobuf.Empty.class,
+      responseType = br.example.demo.model.CustomerProto.Customers.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<com.google.protobuf.Empty,
+      br.example.demo.model.CustomerProto.Customers> getFindAllMethod() {
+    io.grpc.MethodDescriptor<com.google.protobuf.Empty, br.example.demo.model.CustomerProto.Customers> getFindAllMethod;
+    if ((getFindAllMethod = CustomerServiceGrpc.getFindAllMethod) == null) {
+      synchronized (CustomerServiceGrpc.class) {
+        if ((getFindAllMethod = CustomerServiceGrpc.getFindAllMethod) == null) {
+          CustomerServiceGrpc.getFindAllMethod = getFindAllMethod =
+              io.grpc.MethodDescriptor.<com.google.protobuf.Empty, br.example.demo.model.CustomerProto.Customers>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+              .setFullMethodName(generateFullMethodName(SERVICE_NAME, "FindAll"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  com.google.protobuf.Empty.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  br.example.demo.model.CustomerProto.Customers.getDefaultInstance()))
+              .setSchemaDescriptor(new CustomerServiceMethodDescriptorSupplier("FindAll"))
+              .build();
+        }
+      }
+    }
+    return getFindAllMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static CustomerServiceStub newStub(io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<CustomerServiceStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<CustomerServiceStub>() {
+        @java.lang.Override
+        public CustomerServiceStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new CustomerServiceStub(channel, callOptions);
+        }
+      };
+    return CustomerServiceStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static CustomerServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<CustomerServiceBlockingStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<CustomerServiceBlockingStub>() {
+        @java.lang.Override
+        public CustomerServiceBlockingStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new CustomerServiceBlockingStub(channel, callOptions);
+        }
+      };
+    return CustomerServiceBlockingStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static CustomerServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<CustomerServiceFutureStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<CustomerServiceFutureStub>() {
+        @java.lang.Override
+        public CustomerServiceFutureStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new CustomerServiceFutureStub(channel, callOptions);
+        }
+      };
+    return CustomerServiceFutureStub.newStub(factory, channel);
+  }
+
+  /**
+   */
+  public interface AsyncService {
+
+    /**
+     */
+    default void findAll(com.google.protobuf.Empty request,
+        io.grpc.stub.StreamObserver<br.example.demo.model.CustomerProto.Customers> responseObserver) {
+      io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall(getFindAllMethod(), responseObserver);
+    }
+  }
+
+  /**
+   * Base class for the server implementation of the service CustomerService.
+   */
+  public static abstract class CustomerServiceImplBase
+      implements io.grpc.BindableService, AsyncService {
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return CustomerServiceGrpc.bindService(this);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do asynchronous rpc calls to service CustomerService.
+   */
+  public static final class CustomerServiceStub
+      extends io.grpc.stub.AbstractAsyncStub<CustomerServiceStub> {
+    private CustomerServiceStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected CustomerServiceStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new CustomerServiceStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void findAll(com.google.protobuf.Empty request,
+        io.grpc.stub.StreamObserver<br.example.demo.model.CustomerProto.Customers> responseObserver) {
+      io.grpc.stub.ClientCalls.asyncUnaryCall(
+          getChannel().newCall(getFindAllMethod(), getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do synchronous rpc calls to service CustomerService.
+   */
+  public static final class CustomerServiceBlockingStub
+      extends io.grpc.stub.AbstractBlockingStub<CustomerServiceBlockingStub> {
+    private CustomerServiceBlockingStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected CustomerServiceBlockingStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new CustomerServiceBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public br.example.demo.model.CustomerProto.Customers findAll(com.google.protobuf.Empty request) {
+      return io.grpc.stub.ClientCalls.blockingUnaryCall(
+          getChannel(), getFindAllMethod(), getCallOptions(), request);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do ListenableFuture-style rpc calls to service CustomerService.
+   */
+  public static final class CustomerServiceFutureStub
+      extends io.grpc.stub.AbstractFutureStub<CustomerServiceFutureStub> {
+    private CustomerServiceFutureStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected CustomerServiceFutureStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new CustomerServiceFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<br.example.demo.model.CustomerProto.Customers> findAll(
+        com.google.protobuf.Empty request) {
+      return io.grpc.stub.ClientCalls.futureUnaryCall(
+          getChannel().newCall(getFindAllMethod(), getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_FIND_ALL = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final AsyncService serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(AsyncService serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_FIND_ALL:
+          serviceImpl.findAll((com.google.protobuf.Empty) request,
+              (io.grpc.stub.StreamObserver<br.example.demo.model.CustomerProto.Customers>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  public static final io.grpc.ServerServiceDefinition bindService(AsyncService service) {
+    return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+        .addMethod(
+          getFindAllMethod(),
+          io.grpc.stub.ServerCalls.asyncUnaryCall(
+            new MethodHandlers<
+              com.google.protobuf.Empty,
+              br.example.demo.model.CustomerProto.Customers>(
+                service, METHODID_FIND_ALL)))
+        .build();
+  }
+
+  private static abstract class CustomerServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    CustomerServiceBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return br.example.demo.model.CustomerProto.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("CustomerService");
+    }
+  }
+
+  private static final class CustomerServiceFileDescriptorSupplier
+      extends CustomerServiceBaseDescriptorSupplier {
+    CustomerServiceFileDescriptorSupplier() {}
+  }
+
+  private static final class CustomerServiceMethodDescriptorSupplier
+      extends CustomerServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final java.lang.String methodName;
+
+    CustomerServiceMethodDescriptorSupplier(java.lang.String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (CustomerServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new CustomerServiceFileDescriptorSupplier())
+              .addMethod(getFindAllMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
